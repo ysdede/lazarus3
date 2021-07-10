f1c9caa5f499ccab98881f7a93d94bc0bca317a7
ysdede <yunusseyhandede@gmail.com>
2021-07-10 20:28:31 +0300

    Batch importer

---
+++ import.bat
@@ -0,0 +1,13 @@
+jesse import-candles "Binance Futures" BTC-USDT %1
+jesse import-candles "Binance Futures" ETH-USDT %1
+jesse import-candles "Binance Futures" BNB-USDT %1
+jesse import-candles "Binance Futures" LTC-USDT %1
+
+jesse import-candles Binance BTC-USDT %1
+jesse import-candles Binance ETH-USDT %1
+jesse import-candles Binance BNB-USDT %1
+jesse import-candles Binance LTC-USDT %1
+
+jesse import-candles Bitfinex BTC-USD %1
+jesse import-candles Bitfinex ETH-USD %1
+jesse import-candles Bitfinex LTC-USD %1
